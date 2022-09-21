dfb7f78b4c1a613030051905b3efb321db2ee2da
Kyle James <kylecjames01@gmail.com>
2022-09-21 17:18:55 -0400

    Implement Player Movement
    Added basic movement with the mouse where the camera moves with the player.

---
+++ Emu-War/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Fields
+    public float speed;
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Called once per frame; updates the Player object.
+    /// </summary>
+    void Update()
+    {
+        // Movement
+        FollowMouse();
+    }
+
+    /// <summary>
+    /// Enables the Player object to move by following the mouse.
+    /// </summary>
+    private void FollowMouse()
+    {
+        // Get the target position
+        Vector2 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Get the object's current position
+        Vector2 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // Check if the mouse is inside the player
+        bool mouseInsidePlayer = GetComponent<Collider2D>().bounds.Contains(targetPos);
+        
+        // If it isn't, move the player
+        if(!mouseInsidePlayer)
+        {
+            // Transform the Player object toward the target position
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+    }
+    #endregion
+}
