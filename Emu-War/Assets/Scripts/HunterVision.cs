b951b7c09898c6af8f85e96daf3ffdb51fbd6e34
Kyle James <kylecjames01@gmail.com>
2022-10-02 13:11:51 -0400

    Squashed commit of the following:
    commit d48ead8d0512a3d59d193931a126176ffd3fb975
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Sun Oct 2 13:07:12 2022 -0400
    
        Update build
    
    commit 28a1df9631261426e552a35f878c9e041c076a6a
    Author: Ryan Cooper <ryancooper315@gmail.com>
    Date:   Sun Oct 2 12:20:46 2022 -0400
    
        Added new grid textures
    
    commit 8235a5936561e59be9bdfdf4cd43e41dc3937a48
    Author: Ryan Cooper <ryancooper315@gmail.com>
    Date:   Sun Oct 2 01:49:12 2022 -0400
    
        Hunter Patrol Cycle
    
    commit c550888edb500ceed1bbd1cc5c4598739fd07e39
    Author: Ryan Cooper <ryancooper315@gmail.com>
    Date:   Thu Sep 29 01:58:20 2022 -0400
    
        Inital hunter devvelopment
    
        added vision
    
    commit 7c1d67634175a7ddb0111a6b402b9ebedafd982f
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Thu Sep 29 00:18:12 2022 -0400
    
        Update camera movement
    
        Fixed camera movement in browser
    
    commit e50dad76a96557cc93ebd49357944e83fae2a5ed
    Merge: b7506ae c57443c
    Author: Kyle James <43390822+kylejames0408@users.noreply.github.com>
    Date:   Thu Sep 22 12:00:01 2022 -0400
    
        Merge branch 'main' into dev
    
    commit b7506ae69b07fdd11fe24de5d42510a85607f443
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Thu Sep 22 11:58:52 2022 -0400
    
        Update Build
    
        Fix compression issues
    
    commit 97677c91b197dfcd8534d3ceca96ca7753fdb97b
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Thu Sep 22 11:50:56 2022 -0400
    
        Upload Build
    
        Fix build
    
    commit dafc0ea744a409e1ca0c5b04413e8a27386c7fbb
    Author: Kyle James <43390822+kylejames0408@users.noreply.github.com>
    Date:   Thu Sep 22 11:50:28 2022 -0400
    
        Update .gitignore
    
        Include builds.
    
    commit c5a54006a1b29d20369df49d639ae9711471f274
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Thu Sep 22 11:41:34 2022 -0400
    
        WebGL Playtest1 Build
    
        Built to docs folder
    
    commit c0f8a744686d7f4dc5654f9a4158e9bbcb6d9bc7
    Author: Ryan Cooper <ryancooper315@gmail.com>
    Date:   Thu Sep 22 01:12:39 2022 -0400
    
        Added red rock floor texture
    
    commit edad9e509061e978eda96a040de06d44bd576086
    Author: Ryan Cooper <ryancooper315@gmail.com>
    Date:   Wed Sep 21 20:07:39 2022 -0400
    
        Added Created base grid and tile logic
    
        Created the inital grid for our "playtest". Updated Emunel's collision physics.
    
    commit 3c776cbf45f541b3782f6460109a488c2d826503
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Wed Sep 21 18:06:19 2022 -0400
    
        Updated scene
    
    commit 423fcf2a46f7790160c92f9dea22963e866b78f2
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Wed Sep 21 18:01:10 2022 -0400
    
        Player Prefab Update
    
        Updated the prefab to include animations
    
    commit b1f11f38c86b307efd9bd377e6fddf116b8f8154
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Wed Sep 21 17:58:40 2022 -0400
    
        Animated Player
    
        Added Emunuel's walking animation
    
    commit dfb7f78b4c1a613030051905b3efb321db2ee2da
    Author: Kyle James <kylecjames01@gmail.com>
    Date:   Wed Sep 21 17:18:55 2022 -0400
    
        Implement Player Movement
    
        Added basic movement with the mouse where the camera moves with the player.

---
+++ Emu-War/Assets/Scripts/HunterVision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterVision : MonoBehaviour
+{
+    #region fields
+    [SerializeField]
+    private GameObject _hunterAi;
+    #endregion
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Emu")
+        {
+            _hunterAi.GetComponent<AIHunterTracking>().InSight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Emu")
+        {
+            _hunterAi.GetComponent<AIHunterTracking>().InSight = false;
+        }
+    }
+
+
+}
