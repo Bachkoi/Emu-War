9e7a912c244174ce2f7466dbadf581ebb74cfb70
Ryan Cooper <ryancooper315@gmail.com>
2022-10-10 02:43:55 -0400

    Rotation seperate from patrolling movement

--- Emu-War/Assets/Scripts/AIHunterTracking.cs
+++ Emu-War/Assets/Scripts/AIHunterTracking.cs
@@ -6,14 +6,21 @@ public class AIHunterTracking : MonoBehaviour
 {
     #region Fields
     public bool inSight;
+    public float hunterSpeed;
+    [SerializeField]
+    private float _rotationSpeed;
+    #region Patrol Points
     [SerializeField]
     private List<Vector3> _spots;
     private Queue<Vector3> _hotpoints;
-    private float _travelDistance = 4.0f;
     [SerializeField]
     private float _randomSpotOdds;
     private Vector3 _currentNode;
-    public float hunterSpeed;
+    private bool _rotateToPoint;
+    private Vector3 _rotatePoint;
+    private int _rotateTimer;
+    #endregion Patrol Points
+
     #endregion
     #region Properties
     public bool InSight
@@ -29,28 +36,29 @@ public class AIHunterTracking : MonoBehaviour
         _hotpoints = new Queue<Vector3>(_spots);
         _currentNode = _hotpoints.Dequeue();
         _hotpoints.Enqueue(_currentNode);
+        _rotateToPoint = true;
+        _rotateTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         OnSight();
-        transform.position = Vector2.MoveTowards(transform.position, _currentNode, hunterSpeed * Time.deltaTime);
-        //transform.LookAt(_currentNode,Vector3.left);
-        transform.right = _currentNode - transform.position;
-        CheckDistance();
+
+
     }
 
     public void OnSight()
     {
         if (inSight)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+            ShootAtTarget();
+
         }
         else
         {
+            Patrol();
 
-            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 
@@ -61,6 +69,50 @@ public class AIHunterTracking : MonoBehaviour
             _currentNode = _hotpoints.Dequeue();
             Debug.Log(_currentNode);
             _hotpoints.Enqueue(_currentNode);
+            _rotateToPoint = true;
+            //_rotatePoint = (_currentNode - transform.position).normalized;
         }
+    }
+
+    public void Patrol()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+
+        if (_rotateToPoint)
+        {
+            //transform.right += (_currentNode -transform.position).normalized;
+            //Quaternion rotateTo = Quaternion.LookRotation(Vector3.forward, (_currentNode - transform.position).normalized);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, _rotationSpeed * Time.deltaTime);
+            _rotateTimer++;
+
+            //Debug.Log("Right: " + transform.right + " Rotate Point: " + _rotatePoint + " Distance: " + Vector3.Distance(transform.right, _rotatePoint));
+            Vector3 targetOfRotation = _currentNode - transform.position;
+            float angle = Mathf.Atan2(targetOfRotation.y, targetOfRotation.x) * Mathf.Rad2Deg;
+            Quaternion rotationQuaternion = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationQuaternion, _rotationSpeed * Time.deltaTime);
+
+            float dot = Vector2.Dot( (targetOfRotation).normalized, this.transform.forward);
+            Debug.Log(dot);
+            if (_rotateTimer >= 360)
+            {
+                _rotateToPoint = false;
+                _rotateTimer = 0;
+                //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, transform.eulerAngles.z + 90);
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _currentNode, hunterSpeed * Time.deltaTime);
+
+            
+            //float angle = Mathf.Atan2(_)
+            CheckDistance();
+        }
+
+    }
+
+    public void ShootAtTarget()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
     }
 }
